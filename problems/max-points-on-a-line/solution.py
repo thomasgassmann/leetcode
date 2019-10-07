19bd83b3fb14e9766831986a92986fd19e49cbcf
Thomas Gassmann <thomas.gassmann@hotmail.com>
2019-10-07 21:33:09 +0200

    add max-points-on-a-line stub

---
+++ problems/max-points-on-a-line/solution.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        pass
+
+
+if __name__ == '__main__':
+    print(Solution().maxPoints(
+        [[1, 1], [3, 2], [5, 3], [4, 1], [2, 3], [1, 4]]))
+    print(Solution().maxPoints(
+        [[1, 1], [2, 2], [3, 3]]))
